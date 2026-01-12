50f9ff3d2dca1f5592cb80cf18fc03153fd5e5d4
devathedeveloper-jayjagat <dpatidar@trueigtech.com>
2026-01-12 17:04:56 +0530

    feat: displaying todo list

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const inputField = document.querySelector("#todoInput")
 const button = document.querySelector("#addBtn")
-
+const todoListParents = document.querySelector("#todoList")
 
 button.addEventListener("click" , (e) =>{
     task = inputField.value.trim()
@@ -15,7 +15,23 @@ button.addEventListener("click" , (e) =>{
     const todoList = JSON.parse(localStorage.getItem("todoList")) || []
     todoList.push(todo)
     localStorage.setItem("todoList" , JSON.stringify(todoList))
+    const todoItem = document.createElement("li")
+    todoItem.textContent = todo.task
+    todoListParents.appendChild(todoItem)
     inputField.value = ""
     console.log(todoList)
 
 })
+
+
+function displayTodo(){
+    const todoList = JSON.parse(localStorage.getItem("todoList")) || []
+    console.log(todoList)
+    todoList.forEach((todo) => {
+        const todoItem = document.createElement("li")
+        todoItem.textContent = todo.task
+        todoListParents.appendChild(todoItem)
+    })
+}
+
+displayTodo()
