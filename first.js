507d4763aaff73cb0403d00753ca3db817f888a8
devathedeveloper-jayjagat <dpatidar@trueigtech.com>
2026-01-12 15:27:38 +0530

    fix: try-catch block in function execution

--- first.js
+++ first.js
@@ -1,1 +1,17 @@
-const name = "developer";
+
+function greetings(name) {
+    if (typeof name === "string") {
+        return `hello ${name}`;
+    }
+    throw new Error("name must be string");
+}
+
+try{
+    
+    const dev = "Devdeep"
+    let data = greetings(dev);
+    console.log(data);
+}catch (error){
+    console.log("Error: ", error.message);
+}
+
